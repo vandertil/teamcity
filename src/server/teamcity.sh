a4a2d886321f3ab087eacc487f5b4fbab351aca0
Jos van der Til <jos@vandertil.net>
2016-10-12 21:45:26 +0200

    Assert ownership of TC dir on start

--- src/server/teamcity.sh
+++ src/server/teamcity.sh
@@ -6,7 +6,6 @@ echo "connectionProperties.user=$POSTGRES_USER_USERNAME" >> $TEAMCITY_DATA_PATH/
 echo "connectionProperties.password=$POSTGRES_USER_PASSWORD" >> $TEAMCITY_DATA_PATH/config/database.properties
 echo "maxConnections=50" >> $TEAMCITY_DATA_PATH/config/database.properties
 
-
 # Configure HTTP / HTTPS
 if [ $TEAMCITY_ENABLE_HTTPS = 'true' ]; then
 $TEAMCITY_INSTALL_DIR/TeamCity/enable-https.sh
@@ -14,6 +13,7 @@ fi
 
 # Assert ownership of data path
 chown -R teamcity:nogroup $TEAMCITY_DATA_PATH
+chown -R teamcity:nogroup $TEAMCITY_INSTALL_DIR/TeamCity
 
 # Start TeamCity
 su -c "$TEAMCITY_INSTALL_DIR/TeamCity/bin/teamcity-server.sh run" teamcity
